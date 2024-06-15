9300afd34e00534bf338ea96872e33b057688197
Eldar <eldar_osmanov@bk.ru>
2024-06-15 18:59:57 +1000

    linter

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-// import fs from 'fs';
-// import path from 'path';
-// import parser from './parser.js';
-// import compareData from './compare.js';
-// import getFormat from './formatters/index.js';
-
-// const getPath = (way) => path.resolve(process.cwd(), way);
-
-// const getData = (filepath) => {
-//   const resolvedFilepath = getPath(filepath);
-//   const content = fs.readFileSync(resolvedFilepath, 'utf-8');
-//   return parser(content, filepath.split('.').pop());
-// };
-
-// const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-//   const data1 = getData(filepath1);
-//   const data2 = getData(filepath2);
-
-//   const differences = getFormat(compareData(data1, data2), formatName);
-//   return differences;
-// };
-
-// export default genDiff;
-
 import fs from 'fs';
 import path from 'path';
 import parser from './parser.js';
@@ -33,7 +9,7 @@ const getPath = (way) => path.resolve(process.cwd(), way);
 const getData = (filepath) => {
   const resolvedFilepath = getPath(filepath);
   const content = fs.readFileSync(resolvedFilepath, 'utf-8');
-  const extension = path.extname(filepath).slice(1); 
+  const extension = path.extname(filepath).slice(1);
   return parser(content, extension);
 };
 
