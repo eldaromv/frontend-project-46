19decb1bfc0355d03b1ce2c8c721fa8f77d47abb
Eldar <eldar_osmanov@bk.ru>
2024-06-16 01:07:45 +1000

    fixed children mistake

--- src/compare.js
+++ src/compare.js
@@ -1,3 +1,60 @@
+// import _ from 'lodash';
+
+// const sortKeys = (data1, data2) => {
+//   const keys1 = Object.keys(data1);
+//   const keys2 = Object.keys(data2);
+//   const sortedKeys = _.sortBy(_.union(keys1, keys2));
+//   return sortedKeys;
+// };
+
+// const compareData = (obj1, obj2) => {
+//   const keys = sortKeys(obj1, obj2);
+//   return keys.map((key) => {
+//     const obj1HasKey = _.has(obj1, key);
+//     const obj2HasKey = _.has(obj2, key);
+//     const value1 = obj1[key];
+//     const value2 = obj2[key];
+//     const obj1KeyIsObject = (_.isPlainObject(value1));
+//     const obj2KeyIsObject = (_.isPlainObject(value2));
+
+//     if (obj1HasKey && obj2HasKey && obj1KeyIsObject && obj2KeyIsObject) {
+//       return {
+//         type: 'parent',
+//         key,
+//         children: compareData(value1, value2),
+//       };
+//     }
+//     if (obj1HasKey && !obj2HasKey) {
+//       return {
+//         type: 'deleted',
+//         key,
+//         children: value1,
+//       };
+//     }
+//     if (!obj1HasKey && obj2HasKey) {
+//       return {
+//         type: 'added',
+//         key,
+//         children: value2,
+//       };
+//     }
+//     if (obj1HasKey && obj2HasKey && value1 === value2) {
+//       return {
+//         type: 'staySame',
+//         key,
+//         children: value1,
+//       };
+//     }
+//     return {
+//       type: 'diffValue',
+//       key,
+//       children: value1,
+//       children2: value2,
+//     };
+//   });
+// };
+// export default compareData;
+
 import _ from 'lodash';
 
 const sortKeys = (data1, data2) => {
@@ -28,28 +85,28 @@ const compareData = (obj1, obj2) => {
       return {
         type: 'deleted',
         key,
-        children: value1,
+        value: value1,
       };
     }
     if (!obj1HasKey && obj2HasKey) {
       return {
         type: 'added',
         key,
-        children: value2,
+        value: value2,
       };
     }
     if (obj1HasKey && obj2HasKey && value1 === value2) {
       return {
         type: 'staySame',
         key,
-        children: value1,
+        value: value1,
       };
     }
     return {
       type: 'diffValue',
       key,
-      children: value1,
-      children2: value2,
+      value1: value1,
+      value2: value2,
     };
   });
 };
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,3 +1,43 @@
+// import _ from 'lodash';
+
+// const getIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(spacesCount * depth - 2);
+
+// const getValue = (value, depth) => {
+//   if (!_.isPlainObject(value)) {
+//     return value;
+//   }
+//   const keys = Object.keys(value);
+//   const result = keys.map((key) => {
+//     const newKey = value[key];
+//     return `${getIndent(depth + 1)}  ${key}: ${getValue(newKey, depth + 1)}`;
+//   });
+//   return `{\n${result.join('\n')}\n  ${getIndent(depth)}}`;
+// };
+
+// const makeStylish = (array) => {
+//   const iter = (node, depth = 1) => {
+//     const result = node.map((element) => {
+//       if (element.type === 'parent') {
+//         return `${getIndent(depth)}  ${element.key}: {\n${iter(element.children, depth + 1)}\n${getIndent(depth)}  }`;
+//       }
+//       if (element.type === 'staySame') {
+//         return `${getIndent(depth)}  ${element.key}: ${getValue(element.children, depth)}`;
+//       }
+//       if (element.type === 'deleted') {
+//         return `${getIndent(depth)}- ${element.key}: ${getValue(element.children, depth)}`;
+//       }
+//       if (element.type === 'added') {
+//         return `${getIndent(depth)}+ ${element.key}: ${getValue(element.children, depth)}`;
+//       }
+//       return `${getIndent(depth)}- ${element.key}: ${getValue(element.children, depth)}\n${getIndent(depth)}+ ${element.key}: ${getValue(element.children2, depth)}`;
+//     });
+
+//     return result.join('\n');
+//   };
+//   return `{\n${iter(array)}\n}`;
+// };
+// export default makeStylish;
+
 import _ from 'lodash';
 
 const getIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(spacesCount * depth - 2);
@@ -21,15 +61,15 @@ const makeStylish = (array) => {
         return `${getIndent(depth)}  ${element.key}: {\n${iter(element.children, depth + 1)}\n${getIndent(depth)}  }`;
       }
       if (element.type === 'staySame') {
-        return `${getIndent(depth)}  ${element.key}: ${getValue(element.children, depth)}`;
+        return `${getIndent(depth)}  ${element.key}: ${getValue(element.value, depth)}`;
       }
       if (element.type === 'deleted') {
-        return `${getIndent(depth)}- ${element.key}: ${getValue(element.children, depth)}`;
+        return `${getIndent(depth)}- ${element.key}: ${getValue(element.value, depth)}`;
       }
       if (element.type === 'added') {
-        return `${getIndent(depth)}+ ${element.key}: ${getValue(element.children, depth)}`;
+        return `${getIndent(depth)}+ ${element.key}: ${getValue(element.value, depth)}`;
       }
-      return `${getIndent(depth)}- ${element.key}: ${getValue(element.children, depth)}\n${getIndent(depth)}+ ${element.key}: ${getValue(element.children2, depth)}`;
+      return `${getIndent(depth)}- ${element.key}: ${getValue(element.value1, depth)}\n${getIndent(depth)}+ ${element.key}: ${getValue(element.value2, depth)}`;
     });
 
     return result.join('\n');
@@ -37,3 +77,4 @@ const makeStylish = (array) => {
   return `{\n${iter(array)}\n}`;
 };
 export default makeStylish;
+
