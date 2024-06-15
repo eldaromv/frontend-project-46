7b2b88301cd5d077fea2f1b4cb1515a3c9472649
Eldar <eldar_osmanov@bk.ru>
2024-06-15 18:57:43 +1000

    pop mistake in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+// import fs from 'fs';
+// import path from 'path';
+// import parser from './parser.js';
+// import compareData from './compare.js';
+// import getFormat from './formatters/index.js';
+
+// const getPath = (way) => path.resolve(process.cwd(), way);
+
+// const getData = (filepath) => {
+//   const resolvedFilepath = getPath(filepath);
+//   const content = fs.readFileSync(resolvedFilepath, 'utf-8');
+//   return parser(content, filepath.split('.').pop());
+// };
+
+// const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+//   const data1 = getData(filepath1);
+//   const data2 = getData(filepath2);
+
+//   const differences = getFormat(compareData(data1, data2), formatName);
+//   return differences;
+// };
+
+// export default genDiff;
+
 import fs from 'fs';
 import path from 'path';
 import parser from './parser.js';
@@ -9,7 +33,8 @@ const getPath = (way) => path.resolve(process.cwd(), way);
 const getData = (filepath) => {
   const resolvedFilepath = getPath(filepath);
   const content = fs.readFileSync(resolvedFilepath, 'utf-8');
-  return parser(content, filepath.split('.').pop());
+  const extension = path.extname(filepath).slice(1); 
+  return parser(content, extension);
 };
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
